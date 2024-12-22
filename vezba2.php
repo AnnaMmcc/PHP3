cc5be222f21b5ef25fb7f75090ac2f90aef7d80e
AnnaMmcc <167766726+AnnaMmcc@users.noreply.github.com>
2024-12-22 15:38:40 +0100

    Add files via upload

---
+++ vezba2.php
@@ -0,0 +1,12 @@
+<?php
+
+$ime = strtolower("ADministrator");
+
+
+$lozinka = "mojaSifraJeSigurna";
+
+
+if($ime == "administrator" && $lozinka == "mojaSifraJeSigurna")
+{
+    echo "Dobrodosao administratore";
+}
---
+++ vezba3.php
@@ -0,0 +1,99 @@
+<?php
+$trenutnoVreme =6;
+if($trenutnoVreme == 5)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 6)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 7)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 8)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 9)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 10)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 11)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 12)
+{
+    echo "Jutro je";
+}
+if($trenutnoVreme == 13)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 14)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 15)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 16)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 17)
+{
+    echo "Dan je";
+}
+
+if($trenutnoVreme == 18)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 19)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 20)
+{
+    echo "Dan je";
+}
+if($trenutnoVreme == 21)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 22)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 23)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 00)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 1)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 2)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 3)
+{
+    echo "Noc je";
+}
+if($trenutnoVreme == 4)
+{
+    echo "Noc je";
+}
